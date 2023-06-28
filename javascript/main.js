ac04ca5f1bd565bc2b58769518c40dc204fe8ecf
scartage <scartage_estudios@proton.me>
2023-06-28 18:46:59 +0200

    adding columns and rows, filter is mid working

--- javascript/main.js
+++ javascript/main.js
@@ -16,30 +16,93 @@ function callAPI() {
             const res = yield fetch('https://pokeapi.co/api/v2/pokemon');
             data = yield res.json();
             console.log(data.results);
-            const pokemonContainer = document.getElementById('pokemonContainer');
-            data.results.forEach((pokemon) => {
-                const column = document.createElement('div');
-                column.className = 'column';
-                const boxCard = document.createElement('div');
-                boxCard.className = 'box card';
-                const cardContent = document.createElement('div');
-                cardContent.className = 'card-content';
-                const nameElement = document.createElement('p');
-                nameElement.textContent = pokemon.name;
-                const contentElement = document.createElement('div');
-                contentElement.className = 'content';
-                contentElement.textContent = 'Lorem ipsum dolor sit amet';
-                cardContent.appendChild(nameElement);
-                boxCard.appendChild(cardContent);
-                boxCard.appendChild(contentElement);
-                column.appendChild(boxCard);
-                pokemonContainer.appendChild(column);
-                console.log(pokemon.name);
-            });
+            makeCards();
         }
         catch (error) {
             console.error(error);
         }
     });
 }
+/*async function callAPI(){
+   try{
+       const res = await fetch('https://pokeapi.co/api/v2/pokemon')
+       data = await res.json();
+       console.log(data.results);
+       
+       const pokemonContainer = document.getElementById('pokemonContainer');
+       const gridContainer = document.createElement('div');
+       gridContainer.className = 'grid-container';
+       pokemonContainer.appendChild(gridContainer);
+
+       data.results.forEach((pokemon) => {
+          
+           const gridItem = document.createElement('div');
+           gridItem.className = 'grid-item';
+
+           const boxCard = document.createElement('div');
+           boxCard.className = 'box card';
+
+           const cardContent = document.createElement('div');
+           cardContent.className = 'card-content';
+
+           const nameElement = document.createElement('p');
+           nameElement.textContent = pokemon.name;
+
+           const contentElement = document.createElement('div');
+           contentElement.className = 'content';
+           contentElement.textContent = 'Lorem ipsum dolor sit amet';
+
+           cardContent.appendChild(nameElement);
+           boxCard.appendChild(cardContent);
+           boxCard.appendChild(contentElement);
+           gridItem.appendChild(boxCard);
+
+           gridContainer.appendChild(gridItem);
+
+           checkFilter(data);
+           console.log(pokemon.name);
+       });
+       }catch(error){
+       console.error(error);
+   }
+}*/
 callAPI();
+function displayPokemon(pokemonList) {
+    const pokemonContainer = document.getElementById('pokemonContainer');
+    pokemonContainer.innerHTML = '';
+    makeCards();
+}
+function checkFilter(data) {
+    const input = document.querySelector('.input');
+    input.addEventListener('input', function () {
+        const searchTerm = input.value.toLowerCase();
+        const filteredPokemon = data.results.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
+        displayPokemon(filteredPokemon);
+    });
+}
+function makeCards() {
+    const pokemonContainer = document.getElementById('pokemonContainer');
+    const gridContainer = document.createElement('div');
+    gridContainer.className = 'grid-container';
+    pokemonContainer.appendChild(gridContainer);
+    data.results.forEach((pokemon) => {
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        const boxCard = document.createElement('div');
+        boxCard.className = 'box card';
+        const cardContent = document.createElement('div');
+        cardContent.className = 'card-content';
+        const nameElement = document.createElement('p');
+        nameElement.textContent = pokemon.name;
+        const contentElement = document.createElement('div');
+        contentElement.className = 'content';
+        contentElement.textContent = 'Lorem ipsum dolor sit amet';
+        cardContent.appendChild(nameElement);
+        boxCard.appendChild(cardContent);
+        boxCard.appendChild(contentElement);
+        gridItem.appendChild(boxCard);
+        gridContainer.appendChild(gridItem);
+        checkFilter(data);
+        console.log(pokemon.name);
+    });
+}
--- typescript/main.ts
+++ typescript/main.ts
@@ -6,12 +6,27 @@ async function callAPI(){
         const res = await fetch('https://pokeapi.co/api/v2/pokemon')
         data = await res.json();
         console.log(data.results);
+        makeCards();
+        }catch(error){
+        console.error(error);
+    }
+ }
+
+ /*async function callAPI(){
+    try{   
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon')
+        data = await res.json();
+        console.log(data.results);
         
         const pokemonContainer = document.getElementById('pokemonContainer');
+        const gridContainer = document.createElement('div');
+        gridContainer.className = 'grid-container';
+        pokemonContainer.appendChild(gridContainer);
 
         data.results.forEach((pokemon) => {
-            const column = document.createElement('div');
-            column.className = 'column';
+           
+            const gridItem = document.createElement('div');
+            gridItem.className = 'grid-item';
 
             const boxCard = document.createElement('div');
             boxCard.className = 'box card';
@@ -29,16 +44,66 @@ async function callAPI(){
             cardContent.appendChild(nameElement);
             boxCard.appendChild(cardContent);
             boxCard.appendChild(contentElement);
-            column.appendChild(boxCard);
+            gridItem.appendChild(boxCard);
 
-            pokemonContainer.appendChild(column);
+            gridContainer.appendChild(gridItem);
 
+            checkFilter(data);
             console.log(pokemon.name);
         });
         }catch(error){
         console.error(error);
     }
- }
-
+ }*/
 
  callAPI();
+
+ function displayPokemon(pokemonList){
+    const pokemonContainer = document.getElementById('pokemonContainer');
+    pokemonContainer.innerHTML = '';
+    makeCards();
+ }
+
+ function checkFilter(data){
+    const input = document.querySelector('.input');
+    input.addEventListener('input', function(){
+        const searchTerm = input.value.toLowerCase();
+        const filteredPokemon = data.results.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
+        displayPokemon(filteredPokemon);
+    });
+ }
+
+ function makeCards(){
+    const pokemonContainer = document.getElementById('pokemonContainer');
+    const gridContainer = document.createElement('div');
+    gridContainer.className = 'grid-container';
+    pokemonContainer.appendChild(gridContainer);
+
+    data.results.forEach((pokemon) => {
+       
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+
+        const boxCard = document.createElement('div');
+        boxCard.className = 'box card';
+
+        const cardContent = document.createElement('div');
+        cardContent.className = 'card-content';
+
+        const nameElement = document.createElement('p');
+        nameElement.textContent = pokemon.name;
+
+        const contentElement = document.createElement('div');
+        contentElement.className = 'content';
+        contentElement.textContent = 'Lorem ipsum dolor sit amet';
+
+        cardContent.appendChild(nameElement);
+        boxCard.appendChild(cardContent);
+        boxCard.appendChild(contentElement);
+        gridItem.appendChild(boxCard);
+
+        gridContainer.appendChild(gridItem);
+
+        checkFilter(data);
+        console.log(pokemon.name); 
+ }
