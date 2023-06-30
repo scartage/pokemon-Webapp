b6a17c6135afcadd086e0068de3d3156b99f3d49
scartage <scartage_estudios@proton.me>
2023-06-30 12:01:36 +0200

    working on pokemon filters

--- javascript/main.js
+++ javascript/main.js
@@ -10,99 +10,102 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 var data = {};
 const display = document.getElementById("nombre");
+/*async function callAPI(){
+    try{
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon')
+        data = await res.json();
+        console.log(data.results);
+        makeCards();
+        }catch(error){
+            console.error(error);
+    }
+ }*/
+function makingContainer(gridContainer, name, types) {
+    const gridItem = document.createElement('div');
+    gridItem.className = 'grid-item';
+    const boxCard = document.createElement('div');
+    boxCard.className = 'box card';
+    const cardContent = document.createElement('div');
+    cardContent.className = 'card-content';
+    const nameElement = document.createElement('p');
+    nameElement.textContent = name;
+    const contentElement = document.createElement('div');
+    contentElement.className = 'content';
+    contentElement.textContent = types;
+    cardContent.appendChild(nameElement);
+    boxCard.appendChild(cardContent);
+    boxCard.appendChild(contentElement);
+    gridItem.appendChild(boxCard);
+    gridContainer.appendChild(gridItem);
+    console.log(pokemon.name);
+}
+function displaySearch(pokemonContainer, pokemonToShow) {
+    const gridContainer = document.createElement('div');
+    gridContainer.className = 'grid-container';
+    pokemonContainer.appendChild(gridContainer);
+    pokemonToShow.forEach((pokemon) => {
+        makingContainer(gridContainer, pokemon.name, hola);
+    });
+}
+/*pokemonToShow.forEach((pokemon: any) => {
+       fetch(pokemon.url)
+           .then(response => response.json())
+           .then(pokemonData => {
+               const name: string = pokemonData.name;
+               const types: string = pokemonData.types;
+               console.log(`nombre: $(name), Type: $(type)`);
+               makingContainer(gridContainer, name, types);
+           })
+           .catch(error => {
+               console.log("Error al obtener la info");
+           })
+   });*/
+function showPokemons(pokemonToShow, flag) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const pokemonContainer = document.getElementById('pokemonContainer');
+        if (flag == true) {
+            pokemonContainer.innerHTML = '';
+            console.log("limpiar html");
+            displaySearch(pokemonContainer, pokemonToShow);
+        }
+        else {
+            const gridContainer = document.createElement('div');
+            gridContainer.className = 'grid-container';
+            pokemonContainer.appendChild(gridContainer);
+            pokemonToShow.results.forEach((pokemon) => {
+                const response = yield fetch(pokemon.url);
+                const pokemonData = yield responseesponse.json();
+                const name = pokemonData.name;
+                const types = pokemonData.types;
+                makingContainer(gridContainer, name, types);
+                console.log(`Nombre: ${name}, Tipo: ${types}`);
+            });
+        }
+    });
+}
+function checkFilter(data) {
+    const input = document.querySelector('input');
+    input.addEventListener('input', function () {
+        const searchTerm = input.value.toLowerCase();
+        console.log(searchTerm);
+        const filteredPokemon = data.results.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
+        console.log(filteredPokemon);
+        showPokemons(filteredPokemon, true);
+    });
+}
 function callAPI() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const res = yield fetch('https://pokeapi.co/api/v2/pokemon');
             data = yield res.json();
             console.log(data.results);
-            makeCards();
+            showPokemons(data, false);
+            checkFilter(data);
+            console.log("pepe");
         }
         catch (error) {
             console.error(error);
         }
     });
 }
-/*async function callAPI(){
-   try{
-       const res = await fetch('https://pokeapi.co/api/v2/pokemon')
-       data = await res.json();
-       console.log(data.results);
-       
-       const pokemonContainer = document.getElementById('pokemonContainer');
-       const gridContainer = document.createElement('div');
-       gridContainer.className = 'grid-container';
-       pokemonContainer.appendChild(gridContainer);
-
-       data.results.forEach((pokemon) => {
-          
-           const gridItem = document.createElement('div');
-           gridItem.className = 'grid-item';
-
-           const boxCard = document.createElement('div');
-           boxCard.className = 'box card';
-
-           const cardContent = document.createElement('div');
-           cardContent.className = 'card-content';
-
-           const nameElement = document.createElement('p');
-           nameElement.textContent = pokemon.name;
-
-           const contentElement = document.createElement('div');
-           contentElement.className = 'content';
-           contentElement.textContent = 'Lorem ipsum dolor sit amet';
-
-           cardContent.appendChild(nameElement);
-           boxCard.appendChild(cardContent);
-           boxCard.appendChild(contentElement);
-           gridItem.appendChild(boxCard);
-
-           gridContainer.appendChild(gridItem);
-
-           checkFilter(data);
-           console.log(pokemon.name);
-       });
-       }catch(error){
-       console.error(error);
-   }
-}*/
 callAPI();
-function displayPokemon(pokemonList) {
-    const pokemonContainer = document.getElementById('pokemonContainer');
-    pokemonContainer.innerHTML = '';
-    makeCards();
-}
-function checkFilter(data) {
-    const input = document.querySelector('.input');
-    input.addEventListener('input', function () {
-        const searchTerm = input.value.toLowerCase();
-        const filteredPokemon = data.results.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
-        displayPokemon(filteredPokemon);
-    });
-}
-function makeCards() {
-    const pokemonContainer = document.getElementById('pokemonContainer');
-    const gridContainer = document.createElement('div');
-    gridContainer.className = 'grid-container';
-    pokemonContainer.appendChild(gridContainer);
-    data.results.forEach((pokemon) => {
-        const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
-        const boxCard = document.createElement('div');
-        boxCard.className = 'box card';
-        const cardContent = document.createElement('div');
-        cardContent.className = 'card-content';
-        const nameElement = document.createElement('p');
-        nameElement.textContent = pokemon.name;
-        const contentElement = document.createElement('div');
-        contentElement.className = 'content';
-        contentElement.textContent = 'Lorem ipsum dolor sit amet';
-        cardContent.appendChild(nameElement);
-        boxCard.appendChild(cardContent);
-        boxCard.appendChild(contentElement);
-        gridItem.appendChild(boxCard);
-        gridContainer.appendChild(gridItem);
-        checkFilter(data);
-        console.log(pokemon.name);
-    });
-}
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,109 +1,112 @@
 var data = {};
 const display = document.getElementById("nombre");
 
-async function callAPI(){
+/*async function callAPI(){
     try{   
         const res = await fetch('https://pokeapi.co/api/v2/pokemon')
         data = await res.json();
         console.log(data.results);
         makeCards();
         }catch(error){
-        console.error(error);
-    }
- }
-
- /*async function callAPI(){
-    try{   
-        const res = await fetch('https://pokeapi.co/api/v2/pokemon')
-        data = await res.json();
-        console.log(data.results);
-        
-        const pokemonContainer = document.getElementById('pokemonContainer');
-        const gridContainer = document.createElement('div');
-        gridContainer.className = 'grid-container';
-        pokemonContainer.appendChild(gridContainer);
-
-        data.results.forEach((pokemon) => {
-           
-            const gridItem = document.createElement('div');
-            gridItem.className = 'grid-item';
-
-            const boxCard = document.createElement('div');
-            boxCard.className = 'box card';
-
-            const cardContent = document.createElement('div');
-            cardContent.className = 'card-content';
-
-            const nameElement = document.createElement('p');
-            nameElement.textContent = pokemon.name;
-
-            const contentElement = document.createElement('div');
-            contentElement.className = 'content';
-            contentElement.textContent = 'Lorem ipsum dolor sit amet';
-
-            cardContent.appendChild(nameElement);
-            boxCard.appendChild(cardContent);
-            boxCard.appendChild(contentElement);
-            gridItem.appendChild(boxCard);
-
-            gridContainer.appendChild(gridItem);
-
-            checkFilter(data);
-            console.log(pokemon.name);
-        });
-        }catch(error){
-        console.error(error);
+            console.error(error);
     }
  }*/
 
- callAPI();
+function makingContainer(gridContainer, name, types){
+    const gridItem = document.createElement('div');
+    gridItem.className = 'grid-item';
 
- function displayPokemon(pokemonList){
-    const pokemonContainer = document.getElementById('pokemonContainer');
-    pokemonContainer.innerHTML = '';
-    makeCards();
- }
+    const boxCard = document.createElement('div');
+    boxCard.className = 'box card';
 
- function checkFilter(data){
-    const input = document.querySelector('.input');
-    input.addEventListener('input', function(){
-        const searchTerm = input.value.toLowerCase();
-        const filteredPokemon = data.results.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
-        displayPokemon(filteredPokemon);
-    });
- }
+    const cardContent = document.createElement('div');
+    cardContent.className = 'card-content';
 
- function makeCards(){
-    const pokemonContainer = document.getElementById('pokemonContainer');
+    const nameElement = document.createElement('p');
+    nameElement.textContent = name;
+
+    const contentElement = document.createElement('div');
+    contentElement.className = 'content';
+    contentElement.textContent = types;
+
+    cardContent.appendChild(nameElement);
+    boxCard.appendChild(cardContent);
+    boxCard.appendChild(contentElement);
+    gridItem.appendChild(boxCard);
+
+    gridContainer.appendChild(gridItem);
+    console.log(pokemon.name);
+}
+
+function displaySearch(pokemonContainer, pokemonToShow){
     const gridContainer = document.createElement('div');
     gridContainer.className = 'grid-container';
     pokemonContainer.appendChild(gridContainer);
-
-    data.results.forEach((pokemon) => {
-       
-        const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
-
-        const boxCard = document.createElement('div');
-        boxCard.className = 'box card';
-
-        const cardContent = document.createElement('div');
-        cardContent.className = 'card-content';
-
-        const nameElement = document.createElement('p');
-        nameElement.textContent = pokemon.name;
-
-        const contentElement = document.createElement('div');
-        contentElement.className = 'content';
-        contentElement.textContent = 'Lorem ipsum dolor sit amet';
-
-        cardContent.appendChild(nameElement);
-        boxCard.appendChild(cardContent);
-        boxCard.appendChild(contentElement);
-        gridItem.appendChild(boxCard);
-
-        gridContainer.appendChild(gridItem);
-
-        checkFilter(data);
-        console.log(pokemon.name); 
+    pokemonToShow.forEach((pokemon: any) => {
+        makingContainer(gridContainer, pokemon.name, hola);
+    });
  }
+
+
+ /*pokemonToShow.forEach((pokemon: any) => {
+        fetch(pokemon.url)
+            .then(response => response.json())
+            .then(pokemonData => {
+                const name: string = pokemonData.name;
+                const types: string = pokemonData.types;
+                console.log(`nombre: $(name), Type: $(type)`);
+                makingContainer(gridContainer, name, types);
+            })
+            .catch(error => {
+                console.log("Error al obtener la info");
+            })
+    });*/
+
+ async function showPokemons(pokemonToShow, flag: boolean){
+    const pokemonContainer = document.getElementById('pokemonContainer');
+    if (flag == true){
+        pokemonContainer.innerHTML = '';
+        console.log("limpiar html")
+        displaySearch(pokemonContainer, pokemonToShow);
+    }else{
+        const gridContainer = document.createElement('div');
+        gridContainer.className = 'grid-container';
+        pokemonContainer.appendChild(gridContainer);
+        pokemonToShow.results.forEach((pokemon: any) => {
+
+            const response = await fetch(pokemon.url);
+            const pokemonData = await responseesponse.json();
+
+            const name: string = pokemonData.name;
+            const types: string = pokemonData.types;
+            makingContainer(gridContainer, name, types);
+            console.log(`Nombre: ${name}, Tipo: ${types}`);
+        });
+    }
+}
+
+function checkFilter(data){
+    const input = document.querySelector('input');
+    input.addEventListener('input', function(){
+        const searchTerm = input.value.toLowerCase();
+        console.log(searchTerm);
+        const filteredPokemon = data.results.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
+        console.log(filteredPokemon);
+        showPokemons(filteredPokemon, true);
+    });
+ }
+
+async function callAPI(){
+    try{   
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon')
+        data = await res.json()
+        console.log(data.results);
+        showPokemons(data, false);
+        checkFilter(data);
+        console.log("pepe");
+        }catch(error){
+            console.error(error);
+    }
+ }
+
+ callAPI();
